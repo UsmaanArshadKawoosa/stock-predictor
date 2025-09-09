54abad0315c74d94106874b5583644d1692e63e3
UsmaanAK <usmaankawoosa1@gmail.com>
2025-09-09 21:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify
-import joblib
+import numpy as np
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
+model = load_model("intraday_lstm_model.h5")
 
-model = joblib.load('intraday_lstm_model.h5')
+@app.route("/")
+def home():
+    return "Stock Predictor API is running!"
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    input_features = data['features']
-    prediction = model.predict([input_features])
-    return jsonify({'prediction': prediction.tolist()})
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)[0]
+    predicted_class = int(np.round(prediction[0]))
+    return jsonify({"prediction": predicted_class})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
